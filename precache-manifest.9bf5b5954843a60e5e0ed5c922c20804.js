d2494c0c10c3a3a60098e8177207b53705f8e6c7
Moriarty-jim <47500237+Moriarty-jim@users.noreply.github.com>
2020-07-14 12:52:09 +0545

    Updates

---
+++ precache-manifest.9bf5b5954843a60e5e0ed5c922c20804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50285050c04bb3b68ff28a0c001b41d",
+    "url": "/tic-tac-toe-using-react/index.html"
+  },
+  {
+    "revision": "75a8511bc707f7133efc",
+    "url": "/tic-tac-toe-using-react/static/css/main.b24d6e50.chunk.css"
+  },
+  {
+    "revision": "c9fbcb93d38c37c9a57a",
+    "url": "/tic-tac-toe-using-react/static/js/2.d3d17304.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-using-react/static/js/2.d3d17304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a8511bc707f7133efc",
+    "url": "/tic-tac-toe-using-react/static/js/main.9fdbf791.chunk.js"
+  },
+  {
+    "revision": "37acb5e3f0b9bb9e1588",
+    "url": "/tic-tac-toe-using-react/static/js/runtime-main.939e2876.js"
+  },
+  {
+    "revision": "98e79d3732b05739966f383da6ceb160",
+    "url": "/tic-tac-toe-using-react/static/media/paper-you-can-write-on.98e79d37.jpg"
+  }
+]);
